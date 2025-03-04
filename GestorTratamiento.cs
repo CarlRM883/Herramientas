b214f2824e716414405d378b06c0f9f87e367e2c
CarlRM883 <carlos.ramirez883@pascualbravo.edu.co>
2025-03-04 12:59:59 -0500

    Create GestorTratamiento.cs

---
+++ GestorTratamiento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTratatmientos.Model
+{
+    class GestorTratamiento
+    {
+        private SqlConnection enlaceBD;
+        public GestorTratamiento()
+        {
+            enlaceBD = Conexion.AbrirConexion();
+        }
+        public bool AsignarTratamiento(string cedulaPaciente, DateTime fechaAsignado, DateTime fechaInicio, DateTime fechaFinalizacion, string observaciones="")
+        {
+            return true;
+        }
+	
+    }
+}
